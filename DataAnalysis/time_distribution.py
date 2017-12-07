6e5356f72e51d4cbf8e274cf9a9cadee1178ef8f
googlr <googlr@users.noreply.github.com>
2017-12-07 12:15:18 -0500

    Update time_distribution.py

--- DataAnalysis/time_distribution.py
+++ DataAnalysis/time_distribution.py
@@ -1,3 +1,12 @@
+
 from pyspark.sql.functions import substring
 
-crime_df_hour = crime_df.select(substring(crime_df.CMPLNT_FR_TM,1,2).alias('hour')).collect()
+# csvfile = sc.textFile('/user/xg626/NYPD_Complaint_Data_Historic.csv')
+
+# Time Distribution
+## CMPLNT_FR_TM
+hour = sqlContext.sql("SELECT substring(CMPLNT_FR_TM,1,2) AS CMPLNT_FR_HOUR FROM crime ")
+hour.registerTempTable("crime_df_hour")
+
+cmplnt_fr_hour_count = sqlContext.sql("SELECT CMPLNT_FR_HOUR, COUNT(*) AS count FROM crime_df_hour GROUP BY CMPLNT_FR_HOUR")
+cmplnt_fr_hour_count.toPandas().to_csv('cmplnt_fr_hour_count.csv')
