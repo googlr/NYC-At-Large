0d55b01530aca46cff5456e66579b6e1e1bc0aeb
googlr <googlr@users.noreply.github.com>
2017-12-08 12:00:06 -0500

    Update latitude_longitude_clustering.py

--- DataAnalysis/latitude_longitude_clustering.py
+++ DataAnalysis/latitude_longitude_clustering.py
@@ -1,2 +1,14 @@
+from sklearn.cluster import KMeans
+import numpy as np
+
+# Load the data before running the code below
+
 latlnt = sqlContext.sql("SELECT CMPLNT_NUM,LATITUDE,LONGITUDE FROM crime WHERE LATITUDE != '' AND LONGITUDE != '' ")                                      
-latlnt.toPandas().to_csv('latlnt.csv')
+# latlnt.toPandas().to_csv('latlnt.csv')
+
+X = np.array(latlnt.select(latlnt.LATITUDE,latlnt.LONGITUDE).collect())
+# X.shape
+kmeans = KMeans(n_clusters=100, random_state=0).fit(X)
+
+kmeans_labels = kmeans.labels_
+kmeans_cluster_centers = kmeans.cluster_centers_
