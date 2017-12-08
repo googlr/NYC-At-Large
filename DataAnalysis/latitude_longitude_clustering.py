978e237b8d5bf1a8b4daf718750a5b5a6f9de989
googlr <googlr@users.noreply.github.com>
2017-12-08 18:37:40 -0500

    add time measure to cluster

--- DataAnalysis/latitude_longitude_clustering.py
+++ DataAnalysis/latitude_longitude_clustering.py
@@ -1,5 +1,6 @@
 from sklearn.cluster import KMeans
 import numpy as np
+import timeit                                                               
 
 # Load the data before running the code below
 
@@ -8,7 +9,17 @@ latlnt = sqlContext.sql("SELECT CMPLNT_NUM,LATITUDE,LONGITUDE FROM crime WHERE L
 
 X = np.array(latlnt.select(latlnt.LATITUDE,latlnt.LONGITUDE).collect())
 # X.shape
-kmeans = KMeans(n_clusters=100, random_state=0).fit(X)
+# kmeans = KMeans(n_clusters=100, random_state=0).fit(X)
 
+def k_means():
+     start = timeit.default_timer()
+     kmeans = KMeans(n_clusters=100, random_state=0).fit(X)
+     print(kmeans.cluster_centers_)
+     stop = timeit.default_timer()
+     print(stop - start)
+     return kmeans
+
+    
+kmeans = k_means()
 kmeans_labels = kmeans.labels_
 kmeans_cluster_centers = kmeans.cluster_centers_
