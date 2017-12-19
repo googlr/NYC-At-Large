b0a057329611bad05fc72b53fc34b06217cae425
googlr <googlr@users.noreply.github.com>
2017-12-19 18:02:34 -0500

    Update time_distribution.py

--- DataAnalysis/time_distribution.py
+++ DataAnalysis/time_distribution.py
@@ -16,3 +16,7 @@ hour.registerTempTable("crime_df_hour")
 
 cmplnt_fr_hour_count = sqlContext.sql("SELECT CMPLNT_FR_HOUR, COUNT(*) AS count FROM crime_df_hour GROUP BY CMPLNT_FR_HOUR")
 cmplnt_fr_hour_count.toPandas().to_csv('cmplnt_fr_hour_count.csv')
+
+# days
+days = sqlContext.sql("SELECT substring(RPT_DT,1,2) AS MONTH, substring(RPT_DT,4,5) AS DAY, substring(RPT_DT,7,10) AS YEAR, COUNT(*) FROM crime GROUP BY RPT_DT")
+days.toPandas().to_csv('cmplnt_fr_days_count.csv')
